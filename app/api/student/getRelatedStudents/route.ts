8a99417065feaf072bafdabee4023fc193ff7baa
brkhn22 <brkhn2248@gmail.com>
2025-05-21 20:06:36 +0300

    GET Department Related Students

--- app/api/student/getRelatedStudents/route.ts
+++ app/api/student/getRelatedStudents/route.ts
@@ -8,7 +8,6 @@ export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const id = searchParams.get('userId');
   const role = searchParams.get('role');
-
   // 1. Get all advisor data
   if (!id || !role) {
     return NextResponse.json({ message: 'Students not found' }, { status: 404 });
@@ -17,6 +16,47 @@ export async function GET(req: NextRequest) {
   if (role === 'advisor') {
     const students = await prisma.student.findMany({
       where: { advisorId: parseInt(id) },
+      include: {
+        Department: true,
+        Advisor: true,
+        GraduationStatus: true,
+      },
+    });
+    return NextResponse.json(students, { status: 200 });
+  } else if (role === 'department') {
+    // 2. Get all department data
+    const students = await prisma.student.findMany({
+      where: { departmentId: parseInt(id), GraduationStatus: { status: 'ADVISOR_APPROVAL' } },
+      include: {
+        Department: true,
+        Advisor: true,
+        GraduationStatus: true,
+      },
+    });
+    return NextResponse.json(students, { status: 200 });
+  }else if (role === 'faculty') {
+    // 3. Get all faculty data
+    const departments = await prisma.department.findMany({
+      where: { facultyId: parseInt(id) },
+      select: { id: true },
+    });
+    const departmentIds = departments.map((department) => department.id);
+    const students = await prisma.student.findMany({
+      where: { 
+      departmentId: { in: departmentIds },
+      GraduationStatus: { status: 'DEPARTMENT_SECRETARIAT_APPROVAL' },
+      },
+      include: {
+        Department: true,
+        Advisor: true,
+        GraduationStatus: true,
+      },
+    });
+    return NextResponse.json(students, { status: 200 });
+  }else if (role === 'secretariat') {
+    // 4. Get all university data
+    const students = await prisma.student.findMany({
+      where: { GraduationStatus: { status: 'FACULTY_SECRETARIAT_APPROVAL' } },
       include: {
         Department: true,
         Advisor: true,
