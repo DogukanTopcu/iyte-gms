211071a226046862efc938ea10717aab9b5babdb
DogukanTopcu <dogukan_topcu3575@icloud.com>
2025-05-24 23:04:16 +0300

    deployment test

--- app/api/init/createInstitutions/route.ts
+++ app/api/init/createInstitutions/route.ts
@@ -3,6 +3,8 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+export const runtime = 'nodejs';
+
 export async function POST() {
   try {
     // Fetch data from UBYS API
